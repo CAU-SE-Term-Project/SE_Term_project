659a2d1a5c21283229415e111c6b9620bc3b6559
dlgusxor12 <kin3841@gmail.com>
2025-05-09 00:02:18 +0900

    Bug Case2 수정 : 상대 플레이어 버튼 이벤트 삭제

--- src/main/java/YutGame/view/swing/SwingGameViewAdapter.java
+++ src/main/java/YutGame/view/swing/SwingGameViewAdapter.java
@@ -35,6 +35,19 @@ public final class SwingGameViewAdapter implements GameView, GameBoardView.UiCal
 
     @Override
     public void onPieceClicked(int id) {
+        int ownerId = board.getBoardPanel()
+                .getPieceManager()
+                .getPieceView(id)
+                .getOwnerId();
+
+        int currentPlayerId = ctl.getCurrentPlayerId();
+
+        // 디버깅용 나중에 지워도됨
+        if (ownerId != currentPlayerId) {
+            System.out.println("❌ 상대 플레이어의 말 클릭 - 무시됨");
+            return;
+        }
+
         ctl.selectPiece(id);
     }
 
